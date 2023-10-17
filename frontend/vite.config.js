07b95c73ba275c43aecbaf6be79e28e20fc5f796
Moziah7 <Mosiah07007@gmail.com>
2023-10-17 12:46:18 +0300

    frontend | project started
    the project (tick3t) was installed with react + vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: "0.0.0.0"
+  },
+  build: {
+    sourcemap: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
