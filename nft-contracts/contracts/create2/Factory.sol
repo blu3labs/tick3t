d93de9f51e65e57115a1e9e1ea498276c45f3c98
Ahmet <96299039+ahmtguler@users.noreply.github.com>
2023-10-20 11:16:37 +0300

    fee and recipient added to factory

--- nft-contracts/contracts/create2/Factory.sol
+++ nft-contracts/contracts/create2/Factory.sol
@@ -2,11 +2,18 @@
 
 pragma solidity 0.8.20;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721Event} from "../tokens/ERC721/ERC721Event.sol";
 import {ERC1155Event} from "../tokens/ERC1155/ERC1155Event.sol";
 
-contract Factory {
-    address[] public events;
+contract Factory is Ownable {
+    address payable public feeRecipient;
+    uint256 public serviceFee;
+
+    constructor(address payable feeRecipient_, uint256 serviceFee_) Ownable(msg.sender) {
+        feeRecipient = feeRecipient_;
+        serviceFee = serviceFee_;
+    }
 
     event ERC721EventCreated(address indexed eventContract);
     event ERC1155EventCreated(address indexed eventContract);
@@ -15,8 +22,6 @@ contract Factory {
         string memory name,
         string memory uri,
         address payable organizer,
-        address payable feeRecipient,
-        uint256 serviceFee,
         uint256[3] memory prices,
         bytes32 salt
     ) external {
@@ -35,8 +40,6 @@ contract Factory {
         string memory name,
         string memory uri,
         address payable organizer,
-        address payable feeRecipient,
-        uint256 serviceFee,
         uint256[3] memory prices,
         bytes32 salt
     ) external {
@@ -51,7 +54,11 @@ contract Factory {
         emit ERC1155EventCreated(address(eventContract));
     }
 
-    function getAllEvents() external view returns (address[] memory) {
-        return events;
+    function setFeeRecipient(address payable feeRecipient_) external onlyOwner {
+        feeRecipient = feeRecipient_;
+    }
+
+    function setServiceFee(uint256 serviceFee_) external onlyOwner {
+        serviceFee = serviceFee_;
     }
 }
