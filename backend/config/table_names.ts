e6edd800e5e29ad275655c2f0707b8d80322f838
batuhan soylu <bthnsoylu35@gmail.com>
2023-10-21 18:28:45 +0300

    backend || create new db

--- backend/config/table_names.ts
+++ backend/config/table_names.ts
@@ -7,6 +7,6 @@ export interface TableNameI {
 }
 
 export const TABLE_NAMES: TableNameI = {
-    event: "event_421613_242",
-    qr: "qr_421613_244"
+    event: "event_421613_245",
+    qr: "qr_421613_246"
 }
