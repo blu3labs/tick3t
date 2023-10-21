da1c973a912f20013c83eceb5821c08ef4be7ed3
batuhan soylu <bthnsoylu35@gmail.com>
2023-10-21 15:54:33 +0300

    backend || fix

--- backend/index.ts
+++ backend/index.ts
@@ -18,101 +18,145 @@ const event = await eventModel(orm!, db);
 const qr = await qrModel(orm!, db);
 
 app.get("/event/:id", async (req, res) => {
-  const address = req.params.id;
-  const eventData = await event?.First({
-    where: {
-      address: ethers.utils.getAddress(address),
-    },
-  });
-  if (!eventData) {
-    res.json({
-      status: 404,
-      data: "Not Found",
+  try {
+    const address = req.params.id;
+    const eventData = await event?.First({
+      where: {
+        address: ethers.utils.getAddress(address),
+      },
     });
-    return;
+    if (!eventData) {
+      res.json({
+        status: 404,
+        data: "Not Found",
+      });
+      return;
+    }
+    res.json({
+      status: 200,
+      data: eventData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: 500,
+      data: "Unexpected Error",
+    });
   }
-  res.json({
-    status: 200,
-    data: eventData,
-  });
 });
 
 app.get("/all/events", async (req, res) => {
-  const eventData = await event?.All({});
-  // reverse the array
+  try {
+    const eventData = await event?.All({});
+    // reverse the array
 
-  res.json({
-    status: 200,
-    data: eventData,
-  });
+    res.json({
+      status: 200,
+      data: eventData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: 500,
+      data: "Unexpected Error",
+    });
+  }
 });
 
 app.post("/event", async (req, res) => {
-  const body = req.body;
-  const createEvent = await event?.InsertOne({
-    address: body.address,
-    title: body.title,
-    category: body.category,
-    date: body.date,
-    venue: body.venue,
-    image: body.image,
-    description: body.description,
-    venuePrice1: body.venuePrice1,
-    venuePrice2: body.venuePrice2,
-    venuePrice3: body.venuePrice3,
-    chain: body.chain,
-  });
-  res.json({
-    status: 200,
-    data: "success",
-  });
+  try {
+    const body = req.body;
+    const createEvent = await event?.InsertOne({
+      address: body.address,
+      title: body.title,
+      category: body.category,
+      date: body.date,
+      venue: body.venue,
+      image: body.image,
+      description: body.description,
+      venuePrice1: body.venuePrice1,
+      venuePrice2: body.venuePrice2,
+      venuePrice3: body.venuePrice3,
+      chain: body.chain,
+    });
+    res.json({
+      status: 200,
+      data: "success",
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      data: "Unexpected Error",
+    });
+  }
 });
 
 app.post("/qr", async (req, res) => {
-  const body = req.body;
-  const createEvent = await qr?.InsertOne({
-    hash: body.hash,
-    signature: body.signature,
-    owner: body.owner,
-    collection: body.collection,
-    tokenId: body.tokenId,
-    amount: body.amount,
-    deadline: body.deadline,
-    salt: body.salt,
-  });
-  res.json({
-    status: 200,
-    data: "success",
-  });
+  try {
+    const body = req.body;
+    const createEvent = await qr?.InsertOne({
+      hash: body.hash,
+      signature: body.signature,
+      owner: body.owner,
+      collection: body.collection,
+      tokenId: body.tokenId,
+      amount: body.amount,
+      deadline: body.deadline,
+      salt: body.salt,
+    });
+    res.json({
+      status: 200,
+      data: "success",
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      data: "Unexpected Error",
+    });
+  }
 });
 
 app.get("/qr/:id", async (req, res) => {
-  const hash = req.params.id;
-  const qrData = await qr?.First({
-    where: {
-      hash: hash,
-    },
-  });
-  res.json({
-    status: 200,
-    data: qrData,
-  });
+  try {
+    const hash = req.params.id;
+    const qrData = await qr?.First({
+      where: {
+        hash: hash,
+      },
+    });
+    res.json({
+      status: 200,
+      data: qrData,
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      data: "Unexpected Error",
+    });
+  }
 });
 
 app.get("/check/qr", async (req, res) => {
-  const query = req.query
-  const msg = await ValidateTicket(
-    query.owner,
-    query.collection,
-    query.tokenId,
-    query.deadline,
-    query.salt,
-    query.signature
-  );
-  res.json({
-    status: 200,
-    data: msg,
-  });
+  try {
+    const query = req.query;
+    const msg = await ValidateTicket(
+      query.owner as string,
+      query.collection as string, 
+      query.tokenId as string,
+      query.deadline as string,
+      query.salt as string,
+      query.signature as string
+    );
+    res.json({
+      status: 200,
+      data: msg,
+    });
+  } catch (error) {
+    res.json({
+      status: 500,
+      data: "Unexpected Error",
+    });
+  }
 });
 
 app.listen(process.env.PORT || 3001, () => {
